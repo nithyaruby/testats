4b0adf25b77b79a40564ebdb0beadc229eaecb5c
nithyaruby <nithya4ruby@gmail.com>
2013-01-20 22:13:35 +0530

    first comm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testats::Application.config.secret_token = '9c251b177c42fb4a805bf972f989efb95c8bcefe211ce47e36f8d6e3f389b4b081a9cb338318f4076bfb1aab037230f296cccad56a8449e9bf736e5181d5b318'
